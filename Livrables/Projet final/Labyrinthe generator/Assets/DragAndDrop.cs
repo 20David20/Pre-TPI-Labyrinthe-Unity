85c31677fed61b143d3c21e74c76f8a3bb00006c
David Dubey <56110203+20David20@users.noreply.github.com>
2021-03-24 10:35:42 +0100

    Ajout du system de drag and drop

---
+++ Livrables/Projet final/Labyrinthe generator/Assets/DragAndDrop.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject wall;
+    private bool moving;
+
+    private float startPosX;
+    private float startPosY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+            Vector3 mousPos;
+            mousPos = Input.mousePosition;
+            mousPos = Camera.main.ScreenToWorldPoint(mousPos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousPos.x - startPosX, mousPos.y - startPosY, this.gameObject.transform.localPosition.z);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+            moving = true;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        moving = false;
+    }
+}
